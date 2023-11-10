9a9ae55dd8cb5b9664391f0c9862b2408746bf70
Quentin Goujon <quentin.goujon@epita.fr>
2023-11-10 14:02:00 +0100

    [UPDATE] image build and host server :)

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker exec -it ansible ansible-playbook playbook.yml -i inventory/local/hosts
+#docker exec -it ansible ansible-playbook playbook.yml -i inventory/local/hosts
+docker exec -it ansible ansible-playbook  playbook.yml -i inventory/server/hosts -i inventory/local/hosts
